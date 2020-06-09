8588806d02e1a58f59cabbf59a4f8a6f2aac58f1
Philip Querimit <philip@Philips-MacBook-Air.local>
2020-06-08 20:28:48 -0600

    add some styling and song

--- src/components/Vueplaylist.vue
+++ src/components/Vueplaylist.vue
@@ -1,114 +1,125 @@
 <template>
   <div class="music">
-   <header>
-     <h1>My Music Player</h1>
-   </header>
-   <main>
-     <section class="player">
-       <h2 class="song-title">{{ current.title }} - <span>{{ current.artist }}
-         </span></h2>
+    <header>
+      <h1>My Music Player</h1>
+    </header>
+    <main>
+      <section class="player">
+        <h2 class="song-title">
+          {{ current.title }} -
+          <span>{{ current.artist }}</span>
+        </h2>
         <div class="controls">
-         <button class="prev" @click="prev">Prev</button>
-         <button class="play" v-if="!isPlaying" @click="play">Play</button>
-         <button class="pause" v-else @click="pause">Pause</button>
-         <button class="next" @click="next">Next</button>
+          <button class="prev" @click="prev">Prev</button>
+          <button class="play" v-if="!isPlaying" @click="play">Play</button>
+          <button class="pause" v-else @click="pause">Pause</button>
+          <button class="next" @click="next">Next</button>
         </div>
-     </section>
-     <section class="playlist">
-       <h3>The Playlist</h3>
-       <button v-for="song in songs" :key="song.src" @click="play(song)" :class="(song.src 
-       == current.src) ? 'song playing' : 'song'">
-       {{ song.title }} - {{ song.artist }}
-       </button>
-     </section>
-  </main>
- </div>
+      </section>
+      <section class="playlist">
+        <h3>The Playlist</h3>
+        <button
+          v-for="song in songs"
+          :key="song.src"
+          @click="play(song)"
+          :class="(song.src 
+       == current.src) ? 'song playing' : 'song'"
+        >{{ song.title }} - {{ song.artist }}</button>
+      </section>
+    </main>
+  </div>
 </template>
 
 <script>
-
 export default {
-  name: 'Vueplaylist',
-  
-  data (){
+  name: "Vueplaylist",
+
+  data() {
     return {
       current: {},
       index: 0,
       isPlaying: false,
       songs: [
         {
-      title: 'Imagine',
-      artist: 'John Lennon',
-      src: require('../assets/imagine-JohnLennon.mp3')
+          title: "Imagine",
+          artist: "John Lennon",
+          src: require("../assets/imagine-JohnLennon.mp3")
         },
         {
-      title: 'Heal the World',
-      artist: 'Michael Jackson',
-      src: require('../assets/Healtheworld-MichaelJackson.mp3')
-        },
-         {
-      title: 'Four Strong Winds',
-      artist: 'Neil Young',
-      src: require('../assets/FourStrongWinds-NeilYoung.mp3')
+          title: "Heal the World",
+          artist: "Michael Jackson",
+          src: require("../assets/Healtheworld-MichaelJackson.mp3")
         },
         {
-      title: 'Can\'t Help Falling in Love',
-      artist: 'Elvis Presley',
-      src: require('../assets/CantHelpFallingInLove-ElvisPresley.mp3')
+          title: "Four Strong Winds",
+          artist: "Neil Young",
+          src: require("../assets/FourStrongWinds-NeilYoung.mp3")
+        },
+        {
+          title: "Can't Help Falling in Love",
+          artist: "Elvis Presley",
+          src: require("../assets/CantHelpFallingInLove-ElvisPresley.mp3")
+        },
+        {
+          title: "Count on You",
+          artist: "Tommy Shaw",
+          src: require("../assets/CountOnYou-TommyShaw.mp3")
         }
       ],
       player: new Audio()
-    }
+    };
   },
   methods: {
-    play (song) {
-     if (typeof song.src != "undefined") {
-       this.current = song;
+    play(song) {
+      if (typeof song.src != "undefined") {
+        this.current = song;
 
-       this.player.src = this.current.src;
-     }
-       this.player.play();
-       this.player.addEventListener('ended', function () {
-         this.index++;
-         
-         this.index++;
-    if(this.index > this.songs.length -1) {
-         this.index = 0;
-    }
-         this.current = this.song[this.index];
-         this.play(this.current);
-       
-    }.bind(this));
-    this.isPlaying = true;
-    },
+        this.player.src = this.current.src;
+      }
+      this.player.play();
+      this.player.addEventListener(
+        "ended",
+        function() {
+          this.index++;
 
-    pause () {
-        this.player.pause();
-        this.isPlaying = false;    
-        },
-    next () {
-        this.index++;
-        if (this.index > this.songs.length -1) {
+          this.index++;
+          if (this.index > this.songs.length - 1) {
             this.index = 0;
-        }
-        this.current = this.songs[this.index];
-        this.play(this.current);
+          }
+          this.current = this.song[this.index];
+          this.play(this.current);
+        }.bind(this)
+      );
+      this.isPlaying = true;
     },
-    prev (){
-        this.index--;
-        if (this.index < 0) {
-            this.index = this.songs.length -1;
-        }
-        this.current = this.songs[this.index];
-        this.play(this.current);
-    }  
-   },
 
-  created (){
+    pause() {
+      this.player.pause();
+      this.isPlaying = false;
+    },
+    next() {
+      this.index++;
+      if (this.index > this.songs.length - 1) {
+        this.index = 0;
+      }
+      this.current = this.songs[this.index];
+      this.play(this.current);
+    },
+    prev() {
+      this.index--;
+      if (this.index < 0) {
+        this.index = this.songs.length - 1;
+      }
+      this.current = this.songs[this.index];
+      this.play(this.current);
+    }
+  },
+
+  created() {
     this.current = this.songs[this.index];
     this.player.src = this.current.src;
   }
-}
+};
 </script>
 
 <style>
@@ -134,5 +145,76 @@ main {
   margin: 0 auto;
   padding: 50px;
 }
-
+.song-title {
+  color: #212121;
+  font-size: 30px;
+  font-weight: 500;
+  text-transform: uppercase;
+  text-align: center;
+}
+.song-title span {
+  font-weight: 300;
+  font-style: inherit;
+}
+.controls {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 30px 15px;
+}
+button {
+  appearance: none;
+  background: none;
+  border: none;
+  outline: none;
+  cursor: pointer;
+}
+button:hover {
+  opacity: 0.8;
+}
+.play,
+.pause {
+  font-size: 20px;
+  font-weight: 700;
+  padding: 15px 25px;
+  margin: 0px 15px;
+  border-radius: 6px;
+  color: #fff;
+  background-color: #cc2e5d;
+}
+.next,
+.prev {
+  font-size: 16px;
+  font-weight: 700;
+  padding: 10px 20px;
+  margin: 0px 15px;
+  border-radius: 4px;
+  color: #ffffff;
+  background-color: #ff5858;
+}
+.playlist {
+  padding: 20px 30px;
+}
+.playlist h3 {
+  color: #212121;
+  font-size: 28px;
+  font-weight: 400;
+  margin-bottom: 30px;
+  text-align: center;
+}
+.playlist.song {
+  display: block;
+  width: 100%;
+  padding: 15px;
+  font-size: 20px;
+  font-weight: 600;
+  border-radius: 4px;
+}
+.playlist.song:hover {
+  color: #fff;
+}
+.playlist .song.playing {
+  color: #fff;
+  background-image: linear-gradient(to right, #cc2e5d, #ff5858);
+}
 </style>
